574b3011e445227a59d73ddc38eaf895a5443f22
maks.kep.2000@mail.ru <maks.kep.2000@mail.ru>
2024-04-01 17:37:02 +0300

    Add project files.

---
+++ Lesson four/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+
+namespace Lesson_four
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+
+            //Работа НА ЗАНЯТИЕ
+
+            //1) [1,2, 32113, -324, 0 ,4] - найти все четные элементы массива и вывести их на экран
+
+            //int[] mass = new int[] { 1, 2, 32113, -324, 0, 4 };
+            //for (int i = 0;i < mass.Length;i++)
+            //{
+            //    if (mass[i] % 2 == 0)
+            //    {
+            //        Console.WriteLine(mass[i]);
+            //    }
+            //}
+
+
+            //2)[1,2, 32113, -324, 0 ,4] - найти все элементы больше нуля элементы массива и вывести их на экран
+
+            //int[] massTwo = new int[] { 1, 2, 32113, -324, 0, 4 };
+            //for (int i = 0; i < massTwo.Length; i++)
+            //{
+            //    if (massTwo[i] > 0)
+            //    {
+            //        Console.WriteLine(massTwo[i]);
+            //    }
+            //}
+
+            //3)[1,2, 32113, -324, 0 ,4] - найти сумму всех элементов
+
+            //int[] massTwo = new int[] { 1, 2, 3, -7 };
+            //int sum = 0;
+            //for (int i = 0; i < massTwo.Length; i++)
+            //{
+            //    sum += massTwo[i];
+            //}
+            //Console.WriteLine(sum);
+
+            //4) Двумерный массив
+
+            //int[,] array = new int[3, 2] { { 1, 2 }, { 4, 5 }, { 7, 8 } };
+
+            //for (int i = 0; i < array.GetLength(0); i++)
+            //{
+            //   for (int j = 0; j < array.GetLength(1); j++)
+            //    {
+            //        Console.Write(" ");
+            //        Console.Write(array[i, j]);
+            //    }
+            //   Console.WriteLine(); 
+            //}
+
+            //int[] massOne = new int[] { 1, 2, 32113, -324, 0, 4 };
+            //int[] massTwo = new int[] { 6, 7, 323, -324, 8,53 };
+
+            //var truefolse = massOne.Contains(1);
+            //var sadasd = massOne.Distinct().ToList();
+
+            //var containsNumberFive = massTwo.Except(massOne);
+            //foreach (int i in containsNumberFive)
+            //{
+            //    Console.WriteLine(i);
+            //}
+        }
+    }
+}
